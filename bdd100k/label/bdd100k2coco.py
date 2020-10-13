9fb065a3ac409fa4a4c37003d6816a29accf8575
xinw <xinw1012@gmail.com>
2020-10-12 21:33:08 -0700

    update ignore and key names for tracking; fix merge issues

--- bdd100k/label/bdd100k2coco.py
+++ bdd100k/label/bdd100k2coco.py
@@ -1,12 +1,10 @@
 """Convert BDD100K to COCO format.
-
 The annotation files in BDD100K format has additional annotaitons
 ('other person', 'other vehicle' and 'trail') besides the considered
 categories ('car', 'pedestrian', 'truck', etc.) to indicate the uncertain
 regions. Given the different handlings of these additional classes, we
 provide three options to process the labels when converting them into COCO
 format.
-
 1. Ignore the labels. This is the default setting and is often used for
 evaluation. CocoAPIs have native support for ignored annotations.
 https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/cocoeval.py#L370
@@ -71,19 +69,27 @@ def init(
     """Initialze the annotation dictionary."""
     coco: DictObject = defaultdict(list)
     coco["categories"] = [
-        {"supercategory": "none", "id": 1, "name": "pedestrian"},
-        {"supercategory": "none", "id": 2, "name": "rider"},
-        {"supercategory": "none", "id": 3, "name": "car"},
-        {"supercategory": "none", "id": 4, "name": "truck"},
-        {"supercategory": "none", "id": 5, "name": "bus"},
-        {"supercategory": "none", "id": 6, "name": "train"},
-        {"supercategory": "none", "id": 7, "name": "motorcycle"},
-        {"supercategory": "none", "id": 8, "name": "bicycle"},
+        {"supercategory": "human", "id": 1, "name": "pedestrian"},
+        {"supercategory": "human", "id": 2, "name": "rider"},
+        {"supercategory": "vehicle", "id": 3, "name": "car"},
+        {"supercategory": "vehicle", "id": 4, "name": "truck"},
+        {"supercategory": "vehicle", "id": 5, "name": "bus"},
+        {"supercategory": "vehicle", "id": 6, "name": "train"},
+        {"supercategory": "bike", "id": 7, "name": "motorcycle"},
+        {"supercategory": "bike", "id": 8, "name": "bicycle"},
     ]
     if mode == "det":
         coco["categories"] += [
-            {"supercategory": "none", "id": 9, "name": "traffic light"},
-            {"supercategory": "none", "id": 10, "name": "traffic sign"},
+            {
+                "supercategory": "traffic light",
+                "id": 9,
+                "name": "traffic light",
+            },
+            {
+                "supercategory": "traffic sign",
+                "id": 10,
+                "name": "traffic sign",
+            },
         ]
 
     if ignore_as_class:
@@ -177,7 +183,13 @@ def bdd100k2coco_det(
                     annotation["ignore"] = 0
                 else:
                     annotation["ignore"] = int(category_ignored)
-                annotation["id"] = label["id"]
+                # COCOAPIs only ignores the crowd region.
+                annotation["iscrowd"] = annotation["iscrowd"] or int(
+                    category_ignored
+                )
+                # COCO annotation ID starts from 1 instead of 0 as used
+                # in the BDD100K format.
+                annotation["id"] = label["id"] + 1
                 annotation["segmentation"] = [[x1, y1, x1, y2, x2, y2, x2, y1]]
                 coco["annotations"].append(annotation)
         else:
@@ -195,11 +207,14 @@ def bdd100k2coco_track(
     remove_ignore: bool = False,
 ) -> DictObject:
     """Converting BDD100K Tracking Set to COCO format."""
+    # pylint: disable=R1702
     coco, ignore_map, attr_id_dict = init(
         mode="track", ignore_as_class=ignore_as_class
     )
 
-    video_id, image_id, ann_id, global_instance_id = 0, 0, 0, 0
+    # COCO annotation ID starts from 1 instead of 0 as used
+    # in the BDD100K format.
+    video_id, image_id, ann_id, global_instance_id = 1, 1, 1, 1
     no_ann = 0
 
     for video_anns in tqdm(labels):
@@ -208,17 +223,17 @@ def bdd100k2coco_track(
         # videos
         video = dict(id=video_id, name=video_anns[0]["video_name"])
         coco["videos"].append(video)
+        video_name = video_anns[0]["video_name"]
 
         # images
         for image_anns in video_anns:
-            video_name = "-".join(image_anns["name"].split("-")[:-1])
             image = dict(
                 file_name=os.path.join(video_name, image_anns["name"]),
                 height=720,
                 width=1280,
                 id=image_id,
                 video_id=video_id,
-                index=image_anns["index"],
+                frame_id=image_anns["index"],
             )
             coco["images"].append(image)
 
@@ -232,9 +247,9 @@ def bdd100k2coco_track(
                     else:
                         lbl["category"] = ignore_map[lbl["category"]]
                         category_ignored = True
-                    if category_ignored and remove_ignore:
-                        # remove the ignored annotations
-                        continue
+                        if remove_ignore:
+                            # remove the ignored annotations
+                            continue
 
                 bdd100k_id = lbl["id"]
                 if bdd100k_id in instance_id_maps.keys():
@@ -254,11 +269,12 @@ def bdd100k2coco_track(
                     image_id=image_id,
                     category_id=attr_id_dict[lbl["category"]],
                     instance_id=instance_id,
-                    is_occluded=lbl["attributes"]["Occluded"],
-                    is_truncated=lbl["attributes"]["Truncated"],
+                    occluded=lbl["attributes"]["Occluded"],
+                    truncated=lbl["attributes"]["Truncated"],
                     bbox=[x1, y1, x2 - x1, y2 - y1],
                     area=area,
-                    iscrowd=int(lbl["attributes"]["Crowd"]),
+                    iscrowd=int(lbl["attributes"]["Crowd"])
+                    or int(category_ignored),
                     ignore=int(category_ignored),
                     segmentation=[[x1, y1, x1, y2, x2, y2, x2, y1]],
                 )
