7519e90fce200122c5d749a1c8894e9fe302bcb8
Jiangmiao Pang <pangjiangmiao@gmail.com>
2020-10-13 18:49:22 +0800

    fix ci

--- bdd100k/eval/eval_mot.py
+++ bdd100k/eval/eval_mot.py
@@ -15,25 +15,25 @@ import motmetrics as mm
 DictAny = Dict[str, Any]  # type: ignore[misc]
 
 METRIC_MAPS = {
-    'idf1': 'IDF1',
-    'mota': 'MOTA',
-    'motp': 'MOTP',
-    'num_false_positives': 'FP',
-    'num_misses': 'FN',
-    'num_switches': 'IDSw',
-    'mostly_tracked': 'MT',
-    'partially_tracked': 'PT',
-    'mostly_lost': 'ML',
-    'num_fragmentations': 'FM'
+    "idf1": "IDF1",
+    "mota": "MOTA",
+    "motp": "MOTP",
+    "num_false_positives": "FP",
+    "num_misses": "FN",
+    "num_switches": "IDSw",
+    "mostly_tracked": "MT",
+    "partially_tracked": "PT",
+    "mostly_lost": "ML",
+    "num_fragmentations": "FM",
 }
 
 SUPER_CLASSES = {
-    'HUMAN': ['pedestrian', 'rider'],
-    'VEHICLE': ['car', 'truck', 'bus', 'train'],
-    'BIKE': ['motorcycle', 'bicycle']
+    "HUMAN": ["pedestrian", "rider"],
+    "VEHICLE": ["car", "truck", "bus", "train"],
+    "BIKE": ["motorcycle", "bicycle"],
 }
 CLASSES = [c for cs in SUPER_CLASSES.values() for c in cs]
-IGNORE_CLASSES = ['trailer', 'other person', 'other vehicle']
+IGNORE_CLASSES = ["trailer", "other person", "other vehicle"]
 
 
 def parse_args() -> argparse.Namespace:
@@ -41,22 +41,26 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--gt", "-g", help="path to ground truth")
     parser.add_argument(
-        "--result", "-r", help="path to results to be evaluated")
+        "--result", "-r", help="path to results to be evaluated"
+    )
     parser.add_argument(
-        '--iou-thr',
+        "--iou-thr",
         type=float,
         default=0.5,
-        help='iou threshold for mot evaluation')
+        help="iou threshold for mot evaluation",
+    )
     parser.add_argument(
-        '--ignore-iof-thr',
+        "--ignore-iof-thr",
         type=float,
         default=0.5,
-        help='ignore iof threshold for mot evaluation')
+        help="ignore iof threshold for mot evaluation",
+    )
     parser.add_argument(
-        '--nproc',
+        "--nproc",
         type=int,
         default=4,
-        help='number of processes for mot evaluation')
+        help="number of processes for mot evaluation",
+    )
     args = parser.parse_args()
     return args
 
@@ -66,21 +70,22 @@ def parse_objects(objects: List[DictAny]) -> List[np.ndarray]:
     bboxes, labels, ids, ignore_bboxes = [], [], [], []
     for obj in objects:
         bbox = [
-            obj['box2d']['x1'], obj['box2d']['y1'],
-            obj['box2d']['x2'] - obj['box2d']['x1'],
-            obj['box2d']['y2'] - obj['box2d']['y1']
+            obj["box2d"]["x1"],
+            obj["box2d"]["y1"],
+            obj["box2d"]["x2"] - obj["box2d"]["x1"],
+            obj["box2d"]["y2"] - obj["box2d"]["y1"],
         ]
-        if obj['category'] in CLASSES:
-            if 'attributes' in obj and obj['attributes'].get('Crowd', False):
+        if obj["category"] in CLASSES:
+            if "attributes" in obj and obj["attributes"].get("Crowd", False):
                 ignore_bboxes.append(bbox)
             else:
                 bboxes.append(bbox)
-                labels.append(CLASSES.index(obj['category']))
-                ids.append(obj['id'])
-        elif obj['category'] in IGNORE_CLASSES:
+                labels.append(CLASSES.index(obj["category"]))
+                ids.append(obj["id"])
+        elif obj["category"] in IGNORE_CLASSES:
             ignore_bboxes.append(bbox)
         else:
-            raise KeyError('Unknown category.')
+            raise KeyError("Unknown category.")
     return list(map(np.array, [bboxes, labels, ids, ignore_bboxes]))
 
 
@@ -95,32 +100,37 @@ def intersection_over_area(preds: np.ndarray, gts: np.ndarray) -> np.ndarray:
     return out
 
 
-def acc_single_video(gts: List[DictAny],
-                     results: List[DictAny],
-                     iou_thr: float = 0.5,
-                     ignore_iof_thr: float = 0.5) -> List[mm.MOTAccumulator]:
+def acc_single_video(
+    gts: List[DictAny],
+    results: List[DictAny],
+    iou_thr: float = 0.5,
+    ignore_iof_thr: float = 0.5,
+) -> List[mm.MOTAccumulator]:
     """Accumulate results for one video."""
     num_classes = len(CLASSES)
     assert len(gts) == len(results)
-    gts = sorted(gts, key=lambda x: int(x['index']))
-    results = sorted(results, key=lambda x: int(x['index']))
+    gts = sorted(gts, key=lambda x: int(x["index"]))
+    results = sorted(results, key=lambda x: int(x["index"]))
     accs = [mm.MOTAccumulator(auto_id=True) for i in range(num_classes)]
     for gt, result in zip(gts, results):
-        assert gt['index'] == result['index']
-        gt_bboxes, gt_labels, gt_ids, gt_ignores = parse_objects(gt['labels'])
-        pred_bboxes, pred_labels, pred_ids, _ = parse_objects(result['labels'])
+        assert gt["index"] == result["index"]
+        gt_bboxes, gt_labels, gt_ids, gt_ignores = parse_objects(gt["labels"])
+        pred_bboxes, pred_labels, pred_ids, _ = parse_objects(result["labels"])
         for i in range(num_classes):
             gt_inds, pred_inds = gt_labels == i, pred_labels == i
             gt_bboxes_c, gt_ids_c = gt_bboxes[gt_inds], gt_ids[gt_inds]
-            pred_bboxes_c, pred_ids_c = pred_bboxes[pred_inds], pred_ids[
-                pred_inds]
+            pred_bboxes_c, pred_ids_c = (
+                pred_bboxes[pred_inds],
+                pred_ids[pred_inds],
+            )
             if gt_bboxes_c.shape[0] == 0 and pred_bboxes_c.shape[0] != 0:
                 distances = np.full((0, pred_bboxes_c.shape[0]), np.nan)
             elif gt_bboxes_c.shape[0] != 0 and pred_bboxes_c.shape[0] == 0:
                 distances = np.full((gt_bboxes_c.shape[0], 0), np.nan)
             else:
                 distances = mm.distances.iou_matrix(
-                    gt_bboxes_c, pred_bboxes_c, max_iou=1 - iou_thr)
+                    gt_bboxes_c, pred_bboxes_c, max_iou=1 - iou_thr
+                )
             if gt_ignores.shape[0] > 0:
                 # 1. assign gt and preds
                 fps = np.ones(pred_bboxes_c.shape[0]).astype(np.bool)
@@ -142,7 +152,7 @@ def acc_single_video(gts: List[DictAny],
 
 
 def aggregate_accs(
-        accumulators: List[List[mm.MOTAccumulator]]
+    accumulators: List[List[mm.MOTAccumulator]],
 ) -> Tuple[List[List[str]], List[List[mm.MOTAccumulator]], List[str]]:
     """Aggregate the results of the entire dataset."""
     # accs for each class
@@ -151,9 +161,9 @@ def aggregate_accs(
     accs: List[List[str]] = [[] for c in CLASSES]
     for video_ind, _accs in enumerate(accumulators):
         for cls_ind, acc in enumerate(_accs):
-            if len(acc._events['Type']) == 0:
+            if len(acc._events["Type"]) == 0:
                 continue
-            name = f'{CLASSES[cls_ind]}_{video_ind}'
+            name = f"{CLASSES[cls_ind]}_{video_ind}"
             names[cls_ind].append(name)
             accs[cls_ind].append(acc)
 
@@ -164,36 +174,43 @@ def aggregate_accs(
         accs.append([a for c in classes for a in accs[CLASSES.index(c)]])
 
     # overall
-    items.append('OVERALL')
-    names.append([n for name in names[:len(CLASSES)] for n in name])
-    accs.append([a for acc in accs[:len(CLASSES)] for a in acc])
+    items.append("OVERALL")
+    names.append([n for name in names[: len(CLASSES)] for n in name])
+    accs.append([a for acc in accs[: len(CLASSES)] for a in acc])
 
     return names, accs, items
 
 
-def eval_single_class(names: List[str], accs: List[mm.MOTAccumulator]
-                      ) -> List[Union[float, int]]:
+def eval_single_class(
+    names: List[str], accs: List[mm.MOTAccumulator]
+) -> List[Union[float, int]]:
     """Evaluate results for one class."""
     mh = mm.metrics.create()
     summary = mh.compute_many(
-        accs, names=names, metrics=METRIC_MAPS.keys(), generate_overall=True)
-    results = [v['OVERALL'] for k, v in summary.to_dict().items()]
-    motp_ind = list(METRIC_MAPS).index('motp')
+        accs, names=names, metrics=METRIC_MAPS.keys(), generate_overall=True
+    )
+    results = [v["OVERALL"] for k, v in summary.to_dict().items()]
+    motp_ind = list(METRIC_MAPS).index("motp")
     if np.isnan(results[motp_ind]):
         num_dets = mh.compute_many(
             accs,
             names=names,
-            metrics=['num_detections'],
-            generate_overall=True)
-        sum_motp = (summary['motp'] * num_dets['num_detections']).sum()
-        motp = mm.math_util.quiet_divide(sum_motp,
-                                         num_dets['num_detections']['OVERALL'])
+            metrics=["num_detections"],
+            generate_overall=True,
+        )
+        sum_motp = (summary["motp"] * num_dets["num_detections"]).sum()
+        motp = mm.math_util.quiet_divide(
+            sum_motp, num_dets["num_detections"]["OVERALL"]
+        )
         results[motp_ind] = float(1 - motp)
     return results
 
 
-def render_results(summaries: List[List[Union[float, int]]], items: List[str],
-                   metrics: List[str]) -> DictAny:
+def render_results(
+    summaries: List[List[Union[float, int]]],
+    items: List[str],
+    metrics: List[str],
+) -> DictAny:
     """Render the evaluation results."""
     eval_results = pd.DataFrame(columns=metrics)
     # category, super-category and overall results
@@ -203,7 +220,7 @@ def render_results(summaries: List[List[Union[float, int]]], items: List[str],
     # average results
     avg_results: List[Union[int, float]] = []
     for i, m in enumerate(metrics):
-        v = np.array([s[i] for s in summaries[:len(CLASSES)]])
+        v = np.array([s[i] for s in summaries[: len(CLASSES)]])
         v = np.nan_to_num(v, nan=0)
         if dtypes[m] == int:
             avg_results.append(int(v.sum()))
@@ -211,73 +228,81 @@ def render_results(summaries: List[List[Union[float, int]]], items: List[str],
             avg_results.append(float(v.mean()))
         else:
             raise TypeError()
-    eval_results.loc['AVERAGE'] = avg_results
+    eval_results.loc["AVERAGE"] = avg_results
     eval_results = eval_results.astype(dtypes)
 
     strsummary = mm.io.render_summary(
         eval_results,
         formatters=mm.metrics.create().formatters,
-        namemap=METRIC_MAPS)
-    strsummary = strsummary.split('\n')
+        namemap=METRIC_MAPS,
+    )
+    strsummary = strsummary.split("\n")
     assert len(strsummary) == len(CLASSES) + len(SUPER_CLASSES) + 3
-    split_line = '-' * len(strsummary[0])
+    split_line = "-" * len(strsummary[0])
     strsummary.insert(1, split_line)
     strsummary.insert(2 + len(CLASSES), split_line)
     strsummary.insert(3 + len(CLASSES) + len(SUPER_CLASSES), split_line)
-    strsummary = ''.join([f'{s}\n' for s in strsummary])
+    strsummary = "".join([f"{s}\n" for s in strsummary])
     print(strsummary)
 
     outputs: DictAny = dict()
-    for i, item in enumerate(items[len(CLASSES):], len(CLASSES)):
+    for i, item in enumerate(items[len(CLASSES) :], len(CLASSES)):
         outputs[item] = dict()
         for j, metric in enumerate(METRIC_MAPS.values()):
             outputs[item][metric] = summaries[i][j]
-    outputs['OVERALL']['mIDF1'] = eval_results.loc['AVERAGE']['idf1']
-    outputs['OVERALL']['mMOTA'] = eval_results.loc['AVERAGE']['mota']
-    outputs['OVERALL']['mMOTP'] = eval_results.loc['AVERAGE']['motp']
+    outputs["OVERALL"]["mIDF1"] = eval_results.loc["AVERAGE"]["idf1"]
+    outputs["OVERALL"]["mMOTA"] = eval_results.loc["AVERAGE"]["mota"]
+    outputs["OVERALL"]["mMOTP"] = eval_results.loc["AVERAGE"]["motp"]
 
     return outputs
 
 
-def eval_mot(gts: List[List[DictAny]],
-             results: List[List[DictAny]],
-             iou_thr: float = 0.5,
-             ignore_iof_thr: float = 0.5,
-             nproc: int = 4) -> DictAny:
+def eval_mot(
+    gts: List[List[DictAny]],
+    results: List[List[DictAny]],
+    iou_thr: float = 0.5,
+    ignore_iof_thr: float = 0.5,
+    nproc: int = 4,
+) -> DictAny:
     """Evaluate CLEAR MOT metrics for BDD100K."""
-    print('BDD100K tracking evaluation with CLEAR MOT metrics.')
+    print("BDD100K tracking evaluation with CLEAR MOT metrics.")
     t = time.time()
     assert len(gts) == len(results)
     metrics = list(METRIC_MAPS.keys())
 
-    print('accumulating...')
+    print("accumulating...")
     pool = Pool(nproc)
     accs = pool.starmap(
         acc_single_video,
-        zip(gts, results, [iou_thr for _ in range(len(gts))],
-            [ignore_iof_thr for _ in range(len(gts))]))
+        zip(
+            gts,
+            results,
+            [iou_thr for _ in range(len(gts))],
+            [ignore_iof_thr for _ in range(len(gts))],
+        ),
+    )
     names, accs, items = aggregate_accs(accs)
 
-    print('evaluating...')
+    print("evaluating...")
     summaries = pool.starmap(eval_single_class, zip(names, accs))
     pool.close()
 
-    print('rendering...')
+    print("rendering...")
     eval_results = render_results(summaries, items, metrics)
-    print(f'evaluation finishes with {(time.time() - t):.2f} s.')
+    print(f"evaluation finishes with {(time.time() - t):.2f} s.")
     return eval_results
 
 
 def read(inputs: str) -> List[List[DictAny]]:
     """Read annotations from file/files."""
     if osp.isdir(inputs):
-        files = glob.glob(osp.join(inputs, '*.json'))
+        files = glob.glob(osp.join(inputs, "*.json"))
         outputs = [mmcv.load(file) for file in files]
-    elif osp.isfile(inputs) and inputs.endswith('json'):
+    elif osp.isfile(inputs) and inputs.endswith("json"):
         outputs = mmcv.load(inputs)
     else:
-        raise TypeError('Inputs must be a folder or a JSON file.')
-    outputs = sorted(outputs, key=lambda x: str(x[0]['video_name']))
+        raise TypeError("Inputs must be a folder or a JSON file.")
+    outputs = sorted(outputs, key=lambda x: str(x[0]["video_name"]))
     return outputs
 
 
@@ -289,8 +314,9 @@ def main() -> None:
         results=read(args.result),
         iou_thr=args.iou_thr,
         ignore_iof_thr=args.ignore_iof_thr,
-        nproc=args.nproc)
+        nproc=args.nproc,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
