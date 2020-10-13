e5a806b5930fb008f0636ab2142a6f919948809d
Fisher Yu <i@yf.io>
2020-10-13 20:08:18 +0800

    add common/ and clean eval/

---
+++ bdd100k/common/__init__.py
@@ -0,0 +1,1 @@
+"""Common functions and utilities."""
---
+++ bdd100k/common/typing.py
@@ -0,0 +1,6 @@
+"""Common type definitions."""
+
+from typing import Any, Dict
+
+# Used for Json loading
+DictAny = Dict[str, Any]  # type: ignore[misc]
