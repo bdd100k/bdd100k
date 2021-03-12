820385e093c3c58efa02066175748c514de7dc1e
Fisher Yu <i@yf.io>
2021-03-13 00:13:52 +0100

    use typed labels in from_coco.py

--- bdd100k/label/from_coco.py
+++ bdd100k/label/from_coco.py
@@ -1,9 +1,11 @@
 """Convert coco to bdd100k format."""
 import argparse
-import json
 from typing import Any, Dict, List
 
 from pycocotools.coco import COCO
+from scalabel.label.io import save as save_bdd100k
+from scalabel.label.typing import Frame as LabeledFrame
+from scalabel.label.typing import Label
 
 DictAny = Dict[str, Any]  # type: ignore[misc]
 
@@ -26,7 +28,7 @@ def parse_arguments() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def transform(label_file: str) -> List[DictAny]:
+def transform(label_file: str) -> List[LabeledFrame]:
     """Transform to bdd100k format."""
     coco = COCO(label_file)
     img_ids = coco.getImgIds()
@@ -35,44 +37,45 @@ def transform(label_file: str) -> List[DictAny]:
     cats = coco.loadCats(cat_ids)
     nms = [cat["name"] for cat in cats]
     cat_map = dict(zip(coco.getCatIds(), nms))
-    bdd_label = []
+    bdd100k_labels = []
     for img_id in img_ids:
         img = coco.loadImgs(img_id)[0]
         ann_ids = coco.getAnnIds(imgIds=img["id"])
         anns = coco.loadAnns(ann_ids)
-        det_dict = {}
-        det_dict["name"] = img["file_name"]
-        det_dict["url"] = img["coco_url"]
-        det_dict["attributes"] = {
+        det_dict = LabeledFrame()
+        det_dict.name = img["file_name"]
+        det_dict.url = img["coco_url"]
+        det_dict.attributes = {
             "weather": "undefined",
             "scene": "undefined",
             "timeofday": "undefined",
         }
-        det_dict["labels"] = []
+        det_dict.labels = []
         for ann in anns:
-            label = {
-                "id": ann["id"],
-                "category": cat_map[ann["category_id"]],
-                "manualShape": True,
-                "manualAttributes": True,
-                "box2d": {
-                    "x1": ann["bbox"][0],
-                    "y1": ann["bbox"][1],
-                    "x2": ann["bbox"][0] + ann["bbox"][2] - 1,
-                    "y2": ann["bbox"][1] + ann["bbox"][3] - 1,
-                },
-            }
-            det_dict["labels"].append(label)
-        bdd_label.append(det_dict)
-    return bdd_label
+            label = Label(
+                **{
+                    "id": ann["id"],
+                    "category": cat_map[ann["category_id"]],
+                    "manualShape": True,
+                    "manualAttributes": True,
+                    "box2d": {
+                        "x1": ann["bbox"][0],
+                        "y1": ann["bbox"][1],
+                        "x2": ann["bbox"][0] + ann["bbox"][2] - 1,
+                        "y2": ann["bbox"][1] + ann["bbox"][3] - 1,
+                    },
+                }
+            )
+            det_dict.labels.append(label)
+        bdd100k_labels.append(det_dict)
+    return bdd100k_labels
 
 
 def main() -> None:
     """Main."""
     args = parse_arguments()
-    bdd100k_label = transform(args.annFile)
-    with open(args.save_path, "w") as outfile:
-        json.dump(bdd100k_label, outfile)
+    bdd100k_labels = transform(args.annFile)
+    save_bdd100k(args.save_path, bdd100k_labels)
 
 
 if __name__ == "__main__":
