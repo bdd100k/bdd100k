9e851e7e3904fea7a468c22f7910a355814c04aa
Fisher Yu <i@yf.io>
2021-03-16 16:13:29 +0100

    fix windows lint path

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,4 +2,4 @@ python3 -m pylint bdd100k
 python3 -m flake8 --docstring-convention google bdd100k
 python3 -m mypy --strict bdd100k
 python3 -m black --check bdd100k
-python3 -m isort -c bdd100k/**/*.py
+python3 -m isort -c bdd100k
