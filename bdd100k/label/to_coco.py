c6a54ffdcb081679570f67940ee07ec4c841a424
XiaLiPKU <ethanlee@pku.edu.cn>
2021-05-04 03:55:00 -0700

    replace png with jpn for to_coco conversion

--- bdd100k/label/to_coco.py
+++ bdd100k/label/to_coco.py
@@ -309,7 +309,7 @@ def bitmask2coco_ins_seg(
         image_id += 1
         image = ImgType(
             id=image_id,
-            file_name=file_,
+            file_name=file_.replace(".png", ".jpg"),
             height=shape[0],
             width=shape[1],
         )
@@ -360,7 +360,7 @@ def bitmask2coco_seg_track(
                 video_id=video_id,
                 frame_id=frame_id,
                 id=image_id,
-                file_name=file_,
+                file_name=file_.replace(".png", ".jpg"),
                 height=shape[0],
                 width=shape[1],
             )
