ea439f2f0285ad6095e1bdbdc0bf0ed9101f66b9
Anthony Huang <anthony.huang@affirm.com>
2021-12-07 17:30:15 -0500

    Refactoring and add configs

--- bdd100k/label/to_rle.py
+++ bdd100k/label/to_rle.py
@@ -1,24 +1,21 @@
 """Convert bitmask to RLE."""
 import argparse
-import json
 import os
-from typing import List
+from typing import Callable, Dict, List
 
 import numpy as np
 from PIL import Image
-from pycocotools import mask as mask_utils
 from scalabel.common.parallel import NPROC
-from scalabel.common.typing import NDArrayU8
 from scalabel.label.io import load, save
 from scalabel.label.transforms import mask_to_rle
-from scalabel.label.typing import RLE, Dataset, Frame, Label
+from scalabel.label.typing import Category, Dataset, Frame, Label
 from scalabel.label.utils import get_leaf_categories
 
-from ..common.bitmask import parse_bitmask
 from ..common.typing import BDD100KConfig
 from ..common.utils import list_files, load_bdd100k_config
 from ..eval.ins_seg import parse_res_bitmask
-from .to_scalabel import bdd100k_to_scalabel
+
+ToRLEFunc = Callable[[List[Frame], str, List[Category]], None]
 
 
 def parse_args() -> argparse.Namespace:
@@ -66,6 +63,60 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def insseg_to_rle(frames: List[Frame], input: str, categories: List[Category]) -> None:
+    ann_score = {}
+
+    for frame in frames:
+        img_name = frame.name.replace(".jpg", ".png")
+        ann_score[img_name] = []
+        bitmask = np.array(
+            Image.open(os.path.join(input, img_name)),
+            dtype=np.uint8,
+        )
+
+        if frame.labels is None:
+            continue
+        for label in frame.labels:
+            ann_score[img_name].append((label.index, label.score))
+
+        masks, ann_ids, scores, category_ids = parse_res_bitmask(
+            ann_score[img_name], bitmask
+        )
+
+        labels = []
+        for ann_id in ann_ids:
+            label = Label(
+                id=ann_id,
+                category=categories[category_ids[ann_id - 1] - 1].name,
+                score=scores[ann_id - 1],
+            )
+
+            # RLE
+            label.rle = mask_to_rle((masks == ann_id).astype(np.uint8))
+
+            labels.append(label)
+        frame.labels = labels
+
+
+def semseg_to_rle(frames: List[Frame], input: str, categories: List[Category]) -> None:
+    instance_id = 1
+    for frame in frames:
+        frame.labels = []
+        img_name = frame.name
+        bitmask = np.array(
+            Image.open(os.path.join(input, img_name)),
+            dtype=np.uint8,
+        )
+        category_ids = np.unique(bitmask[bitmask > 0])
+
+        for category_id in category_ids:
+            label = Label(id=str(instance_id))
+            label.category = categories[category_id - 1].name
+            label.rle = mask_to_rle((bitmask == category_id).astype(np.uint8))
+            frame.labels.append(label)
+            instance_id += 1
+
+
 def main() -> None:
     args = parse_args()
 
@@ -83,42 +134,14 @@ def main() -> None:
 
     categories = get_leaf_categories(bdd100k_config.scalabel.categories)
 
+    convert_funcs: Dict[str, ToRLEFunc] = dict(
+        sem_seg=semseg_to_rle,
+        ins_seg=insseg_to_rle,
+    )
+
     if args.mode == "ins_seg":
         assert args.score_file is not None
-
         dataset = load(args.score_file)
-        ann_score = {}
-
-        for frame in dataset.frames:
-            img_name = frame.name.replace(".jpg", ".png")
-            ann_score[img_name] = []
-            bitmask = np.array(
-                Image.open(os.path.join(args.input, img_name)),
-                dtype=np.uint8,
-            )
-
-            if frame.labels is None:
-                continue
-            for label in frame.labels:
-                ann_score[img_name].append((label.index, label.score))
-
-            masks, ann_ids, scores, category_ids = parse_res_bitmask(
-                ann_score[img_name], bitmask
-            )
-
-            labels = []
-            for ann_id in ann_ids:
-                label = Label(
-                    id=ann_id,
-                    category=categories[category_ids[ann_id - 1] - 1].name,
-                    score=scores[ann_id - 1],
-                )
-
-                # RLE
-                label.rle = mask_to_rle((masks == ann_id).astype(np.uint8))
-
-                labels.append(label)
-            frame.labels = labels
     else:  # sem_seg
         files = list_files(args.input)
         frames = []
@@ -130,22 +153,7 @@ def main() -> None:
 
         dataset = Dataset(frames=frames)
 
-        instance_id = 1
-        for frame in dataset.frames:
-            img_name = frame.name
-            bitmask = np.array(
-                Image.open(os.path.join(args.input, img_name)),
-                dtype=np.uint8,
-            )
-            category_ids = np.unique(bitmask[bitmask > 0])
-
-            for category_id in category_ids:
-                label = Label(id=str(instance_id))
-                label.category = categories[category_id - 1]
-                label.rle = mask_to_rle((bitmask == category_id).astype(np.uint8))
-                frame.labels.append(label)
-                instance_id += 1
-
+    convert_funcs[args.mode](dataset.frames, args.input, categories)
     save(args.output, dataset.frames)
 
 
